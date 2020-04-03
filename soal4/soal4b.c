3b73d6ca64cbf00f8a2aed8755bf729983381ac8
agung56 <50097116+agung56@users.noreply.github.com>
2020-04-03 16:10:30 +0700

    Add files via upload

--- soal4/soal4b.c
+++ soal4/soal4b.c
@@ -8,7 +8,24 @@
 #include<pthread.h>
 #include<string.h>
 
-void factorialgantitambah(){
+
+int row=0;
+int K[4][5];
+void* factorialgantitambah(void* arg){
+  int i= *((int*)arg);
+  free(arg);
+
+  int total=0;
+  for(int a=i; a>0; a--){
+    total=total+j;
+  }
+
+  if(row > 4){
+    printf("\n");
+    row=0;
+  }
+  printf("%4d", total);
+  row++;
 
 }
 int main(){
@@ -16,5 +33,22 @@ int main(){
   int shmid = shmget(key, sizeof(int)*i*j, IPC_CREAT | 0666);
   matriks = (int *)shmat(shmid, NULL, 0);
 
+  printf("Hasil Perkalian Matrix: \n");
+  for(int b=0; b<i; b++){
+    for(int c=0; c<j ; c++){
+      printf("%2d\t", K[b][c]);
+    }
+    printf("\n");
+  }
+
+  printf("hasil setelah ditambah: \n");
+  pthread_t tid[20];
+
+
+
+  sleep(10);
+
+  shmdt(matriks);
+  shmctl(shmid, IPC_RMID, NULL);
 
 }
